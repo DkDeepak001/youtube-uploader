aab26a8eb9d36cad115d4ed9bbae64fa205740c7
DkDeepak001 <Deepakeswar001@gmail.com>
2023-08-30 19:59:56 +0530

    feat: increase padding

--- src/components/channelDetail.tsx
+++ src/components/channelDetail.tsx
@@ -4,7 +4,7 @@ export default function ChannelDetail() {
   const { data: channelData } = api.youtube.getChannel.useQuery();
   console.log(channelData);
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-y-3">
       <div className="flex flex-row items-center  gap-x-3 py-2">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
@@ -25,23 +25,27 @@ export default function ChannelDetail() {
           </p>
         </div>
       </div>
-      {channelData?.videos?.map((video) => (
-        <div
-          className="flex flex-row  gap-x-10 py-2"
-          key={video?.snippet?.title}
-        >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src={video?.snippet?.thumbnails?.default?.url!}
-            className="h-28 w-36 rounded-lg"
-            alt=""
-          />
-          <div className="mt-3 flex h-full flex-col gap-y-1">
-            <h1 className="text-2xl font-bold">{video?.snippet?.title}</h1>
-            <p className="text-base ">{video?.snippet?.description}</p>
+      <div className="flex flex-row flex-wrap  gap-y-10 ">
+        {channelData?.videos?.map((video) => (
+          <div
+            className="flex w-1/4  flex-col items-center rounded-lg"
+            key={video?.snippet?.title}
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src={video?.snippet?.thumbnails?.default?.url!}
+              className="h-36 w-48 rounded-lg"
+              alt=""
+            />
+            <div className="mt-3 flex h-full flex-col gap-y-1">
+              <h1 className="text-center text-xl font-bold">
+                {video?.snippet?.title}
+              </h1>
+              <p className="text-base ">{video?.snippet?.description}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
