1f5661c6ec08199a784dfa5d802ea89e112b9759
DkDeepak001 <Deepakeswar001@gmail.com>
2023-09-03 13:03:08 +0530

    feat: rework history page

--- src/components/tabel.tsx
+++ src/components/tabel.tsx
@@ -31,7 +31,7 @@ const TableBody = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <tbody
     ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
+    className={cn("[&_tr:last-child]:border-b", className)}
     {...props}
   />
 ));
@@ -56,7 +56,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      "border-b border-b-foreground/40 transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
       className
     )}
     {...props}
--- src/pages/upload/[id].tsx
+++ src/pages/upload/[id].tsx
@@ -1,5 +1,15 @@
 import { useRouter } from "next/router";
 import React from "react";
+import { Badge } from "~/components/badge";
+import { Button } from "~/components/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/tabel";
 import { api } from "~/utils/api";
 
 const VideoStatus = () => {
@@ -14,41 +24,121 @@ const VideoStatus = () => {
 
   return (
     <div className="felx flex-col">
-      <div className="flex flex-row items-center gap-x-16 rounded-lg bg-gray-500 p-5">
+      <div className="flex flex-row items-center gap-x-10 rounded-lg bg-foreground p-5">
         <div className="flex flex-col gap-y-2">
-          <h1 className="text-xl font-bold text-white">Video Status</h1>
-          <h2 className="text-lg font-bold text-white">{video?.status}</h2>
+          <h1 className="text-lg font-semibold text-white">Video Status</h1>
+          <Badge
+            variant="default"
+            className="items-center justify-center py-1 text-center"
+          >
+            {video?.status}
+          </Badge>
         </div>
         <div className="flex flex-col gap-y-2">
-          <h1 className="text-xl font-bold text-white"> Editor</h1>
+          <h1 className="text-lg font-semibold text-white"> Editor</h1>
           <div className="flex flex-row items-center gap-x-2">
             <img
               src={video?.editor?.image}
               alt="profile"
               className="h-8 w-8 rounded-full"
             />
-            <h2 className="text-lg font-bold text-white">
+            <h2 className="text-lg font-bold text-white/80">
               {video?.editor?.name}
             </h2>
           </div>
         </div>
         <div className="flex flex-col gap-y-2">
-          <h1 className="text-xl font-bold text-white">Due Date</h1>
-          <h2 className="text-lg font-bold text-white">
+          <h1 className="text-lg font-semibold text-white">Due Date</h1>
+          <h2 className="text-base font-medium text-white/80">
             {new Date(video?.dueDate).toLocaleDateString()}
           </h2>
         </div>
         <div className="flex flex-col gap-y-2">
-          <h1 className="text-xl font-bold text-white">Original Url</h1>
+          <h1 className="text-lg font-semibold text-white">Original Url</h1>
           <div
             className="w-[500px]  cursor-pointer truncate"
             onClick={() => window.open(video?.videoUrl, "_blank")}
           >
-            <p className="truncate text-xl text-white">{video?.videoUrl}</p>
+            <p className="text-baes truncate text-white/80">
+              {video?.videoUrl}
+            </p>
           </div>
         </div>
       </div>
-      <table className="my-3 w-full table-auto p-2">
+
+      <Table className="mt-5">
+        {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+        <TableHeader>
+          <TableRow>
+            <TableHead className="p-3">Id</TableHead>
+            <TableHead className="p-3">Edited At</TableHead>
+            <TableHead className="p-3">Video</TableHead>
+            <TableHead className="p-3">Approve</TableHead>
+            <TableHead className="p-3">Rework</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {video?.length === 0 ? (
+            <TableRow>
+              <TableCell className="font-medium">No Vidoes</TableCell>
+            </TableRow>
+          ) : (
+            video?.rework?.map((v, index) => (
+              <TableRow key={v.id}>
+                <TableCell className="p-3">{index + 1}</TableCell>
+                <TableCell className="p-3">
+                  {" "}
+                  {new Date(v?.createdAt).toLocaleDateString()}
+                </TableCell>
+                <TableCell className="p-3">
+                  <Button
+                    variant="destructive"
+                    onClick={() => void window.open(v?.videoUrl)}
+                  >
+                    View
+                  </Button>
+                </TableCell>
+                <TableCell className="p-3">
+                  {index === video.rework.length - 1 &&
+                  video.status !== "APPROVED" ? (
+                    <Button
+                      className=" bg-green-500 px-5 py-2 text-white hover:bg-green-700"
+                      onClick={() =>
+                        changeStatus({ id: video.id, status: "APPROVED" })
+                      }
+                    >
+                      Approve
+                    </Button>
+                  ) : (
+                    <Badge variant="secondary" className="text-center">
+                      _____________
+                    </Badge>
+                  )}
+                </TableCell>
+                <TableCell className="p-3">
+                  {index === video.rework.length - 1 &&
+                  video.status !== "REWORK" ? (
+                    <Button
+                      variant="secondary"
+                      onClick={() =>
+                        changeStatus({ id: video.id, status: "REWORK" })
+                      }
+                    >
+                      Ask For Rework
+                    </Button>
+                  ) : (
+                    <Badge variant="secondary" className="text-center">
+                      _____________
+                    </Badge>
+                  )}
+                </TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+
+      {/* <table className="my-3 w-full table-auto p-2">
         <thead className="border-b-2 border-gray-400">
           <tr className="bg-slate-400 p-2 text-left">
             <th className="p-3">Id</th>
@@ -116,7 +206,7 @@ const VideoStatus = () => {
             ))
           )}
         </tbody>
-      </table>
+      </table> */}
     </div>
   );
 };
